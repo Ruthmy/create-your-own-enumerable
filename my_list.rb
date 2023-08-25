463e1c6d90ccdef68d8adb7f97394b40c9786f40
Carmen Bravo <nbravoalcala@gmail.com>
2023-08-25 13:37:47 -0400

    Add code to test the enumerable

--- my_list.rb
+++ my_list.rb
@@ -10,3 +10,24 @@ class MyList
     @list.each(&code_block) if block_given?
   end
 end
+
+# Create a new list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+p(list.all? { |e| e < 5 })
+# output: true
+
+p(list.all? { |e| e > 5 })
+# output: false
+
+# Test #any?
+p(list.any? { |e| e == 2 })
+# output: true
+
+p(list.any? { |e| e == 5 })
+# output: false
+
+# Test #filter
+p(list.filter(&:even?))
+# output: [2, 4]
